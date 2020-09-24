301d1b4bf14df3f5dcbc59f67b9eff3a58ea4496
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-24 10:20:27 +0530

    Added UC3

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -49,3 +49,33 @@ class EmpDailyWage {
 		}
 	}
 }
+
+//UC3
+class EmpDailyWagePartTime {
+	//Constants
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
+		public static final int EMP_RATE_PER_HOUR = 20;
+		public static final int NO_OF_EMPLOYEES = 10;
+		
+		public static void main(String[] args) {
+			//Variables
+			int empHrs = 0;
+			int empWage =0;
+			//Computation
+			for(int i=0;i<NO_OF_EMPLOYEES;i++) {
+				double empcheck = Math.floor(Math.random() * 10) % 2;
+				if(empcheck == IS_FULL_TIME) {
+					empHrs = 8;
+				}
+				else if(empcheck == IS_PART_TIME) {
+					empHrs = 4;
+				}
+				else {
+					empHrs = 0;
+				}
+				empWage = empHrs * EMP_RATE_PER_HOUR;
+				System.out.println("Employee wage : " + empWage);
+			}
+		}
+}
