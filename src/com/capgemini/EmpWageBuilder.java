16a254c374ea0a3e6d940edc8c177bb4e2144da9
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-24 10:25:03 +0530

    Added UC5

--- src/com/capgemini/EmpWageBuilder.java
+++ src/com/capgemini/EmpWageBuilder.java
@@ -110,3 +110,39 @@ class EmpDailyWageSwitchCase {
 		}
 	}
 }
+
+//UC5
+class EmpMonthlyWage {
+	//Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR =20;
+	public static final int NO_OF_WORKING_DAYS =20;
+	public static final int NO_OF_EMPLOYEES = 10;
+	
+	public static void main(String[] args) {
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		//Computation
+		for(int i=0;i<NO_OF_EMPLOYEES;i++) {
+			for(int j=0;j<NO_OF_WORKING_DAYS;j++) {
+				int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+				switch(empcheck) {
+					case IS_PART_TIME:
+						empHrs = 4;
+						break;
+					case IS_FULL_TIME:
+						empHrs = 8;
+						break;
+					default:
+						empHrs = 0;
+				}
+				empWage = empHrs * EMP_RATE_PER_HOUR;
+				totalEmpWage += empWage;
+			}
+			System.out.println("Employee wage : " + totalEmpWage);
+		}
+	}
+}
